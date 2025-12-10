6d4550db01f9ffc5d0cef75e655b6fa0dd1c4551
lamowysecond <lamowyutopieckontakt@gmail.com>
2025-12-10 07:42:44 +0100

    Delete lt7core directory

--- lt7core/util/config/ServerRank.kt
+++
@@ -1,5 +0,0 @@
-package pl.lamas.lt7core.util.config
-
-enum class ServerRank {
-    DEFAULT, LAMA, CHRUMKA, UTOPIEC
-}
--- lt7core/util/gui/GuiButton.kt
+++
@@ -1,7 +0,0 @@
-package pl.lamas.lt7core.util.gui
-
-data class GuiButton(
-    val slot: Int,
-    val icon: org.bukkit.inventory.ItemStack,
-    val onClick: ((event: org.bukkit.event.inventory.InventoryClickEvent) -> Unit)? = null
-)
