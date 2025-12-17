a8d33d37aa421218a5652fb9d7ef0a3b78698159
lamowysecond <lamowyutopieckontakt@gmail.com>
2025-12-17 07:47:27 +0100

    Delete lt7core directory

--- lt7core/util/economy/ShopItem.kt
+++
@@ -1,9 +0,0 @@
-package pl.lamas.lt7core.util.economy
-
-import org.bukkit.inventory.ItemStack
-
-data class ShopItem(
-    val item: ItemStack,
-    val buyPrice: Double,
-    val sellPrice: Double
-)
